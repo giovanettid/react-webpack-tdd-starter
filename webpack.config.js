8321c27a9c4c31dabdea7402eb843b42fd8f4b73
didier <didier.giovanetti@gmail.com>
2017-10-13 00:21:30 +0200

    webpack: configure babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,21 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'build'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
 };
