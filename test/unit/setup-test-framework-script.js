4f2e68873edee465f80579dde7265cb85734330d
didier <didier.giovanetti@gmail.com>
2017-10-30 15:17:29 +0100

    test: configure enzyme & raf polyfill

--- test/unit/setup-test-framework-script.js
+++ test/unit/setup-test-framework-script.js
@@ -1,4 +1,11 @@
+import 'raf/polyfill';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import { expect } from 'chai';
 
 global.expect = expect;
+global.shallow = shallow;
 
+configure({ adapter: new Adapter() });
