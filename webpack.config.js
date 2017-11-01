fa20f930bfe9fc22510116d4ed23f84de182c221
didier <didier.giovanetti@gmail.com>
2017-11-01 21:43:03 +0100

    webpack: configure clean plugin & remove prebuild npm script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -73,6 +74,7 @@ const config = {
     contentBase: path.join(__dirname, 'src'),
   },
   plugins: [
+    new CleanWebpackPlugin(['build']),
     new HtmlWebpackPlugin({
       title: 'js starter',
       favicon: './src/images/favicon.ico',
