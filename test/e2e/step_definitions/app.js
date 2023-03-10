6e4c8814df7cc4ff53a9f51dfbc774b079a3d8a1
Didier Giovanetti <didier.giovanetti@gmail.com>
2023-03-10 11:45:37 +0100

    wdio: update dependencies & move to async

--- test/e2e/step_definitions/app.js
+++ test/e2e/step_definitions/app.js
@@ -1,9 +1,9 @@
-import { When, Then } from 'cucumber';
+import { When, Then } from '@cucumber/cucumber';
 
-When(/^Go js starter$/, () => {
-  browser.url('/');
+When(/^Go js starter$/, async () => {
+  await browser.url('/');
 });
 
-Then(/^Says "(.*)"$/, (starter) => {
-  expect($('.starter').getText()).to.equal(starter);
+Then(/^Says "(.*)"$/, async (starter) => {
+  expect(await $('.starter').getText()).to.equal(starter);
 });
